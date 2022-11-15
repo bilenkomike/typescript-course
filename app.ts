c95907c197c23fb694e2d8b2641b44fbde5ae046
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-15 17:40:36 +0200

    objects, tuples(role field), enum types

--- app.ts
+++ app.ts
@@ -1,10 +1,22 @@
+enum Role {
+  ADMIN = 1,
+  READ_ONLY,
+  AUTHOR,
+}
+
 type Person = {
   name: string;
   age: number;
+  hobbies: string[];
+  roles: [number, string];
+  role: Role;
 };
 
 const person: Person = {
   name: "Miek",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
+  roles: [1, "number"],
+  role: Role.AUTHOR,
 };
 console.log(person);
